9aba818d40bb8bb02d522cb9e4a6367301a2de0c
iposton <ianposton@sbcglobal.net>
2015-05-26 17:45:54 -0700

    fix meteor publish function

--- simple-todos.js
+++ simple-todos.js
@@ -124,18 +124,15 @@ Meteor.methods({
 
 });
 
-Meteor.publish("tasks", function () {
-  return Tasks.find({
+if (Meteor.isServer) {
+  // Only publish tasks that are public or belong to the current user 
+  Meteor.publish("tasks", function () {
+    return Tasks.find({
     $or: [
       { private : {$ne: true} },
       { owner: this.userId }
     ]
-  });
-});
-
-if (Meteor.isServer) {
-  Meteor.publish("tasks", function () {
-    return Tasks.find();
-  });
+   });
+ });
 }
 
